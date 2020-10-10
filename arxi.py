ea0a45598d2f9719ac852af968e27a3a7fadd3c0
Jackson Henry <jacksonhenry3@gmail.com>
2020-10-10 15:29:01 -0400

    reorganized

--- arxi.py
+++
@@ -1,8 +0,0 @@
-import urllib.request as libreq
-with libreq.urlopen('http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=5') as url:
-  r = url.read()
-# print()
-f = open("demofile3.xml", "w")
-# print( )
-f.write(r.decode("utf-8"))
-f.close()
