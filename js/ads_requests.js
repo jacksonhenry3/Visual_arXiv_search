254e70d6aaab4ab8722a7ad7eb490478a7d75458
Nico <nicoadams000@gmail.com>
2020-10-11 14:49:39 -0500

    ads_requests upgrade

--- js/ads_requests.js
+++ js/ads_requests.js
@@ -1,8 +1,17 @@
+// Example: searchRequestFromString('"clumpy galaxies" candels year:2015')
+
 const token = "dnp7EQ5y47A222l6fOc0XCbE3YCPrb6egKygc56I"
 
-function testSearchRequest(searchString) {
+function assembleSearchURL(searchString) {
+  url = "https://api.adsabs.harvard.edu/v1/search/query?q="
+  url += encodeURI(searchString)
+  url += "&fl=title,bibcode,citation_count,year"
+  return url
+}
+
+function searchRequestFromString(searchString) {
   result = $.ajax({
-    url: "https://api.adsabs.harvard.edu/v1/search/query?q="+encodeURI(searchString),
+    url: assembleSearchURL(searchString),
     type: "GET",
     headers: {
         "Access-Control-Allow-Origin":"*",
@@ -17,4 +26,16 @@ function testSearchRequest(searchString) {
     }
   })
   return result
+}
+
+function searchRequest(fields) {
+  searchString = ""
+  for(var key in fields) {
+    if(key == 'searchstr') {
+      searchString += fields[key] + " "
+    } else {
+      searchString += key + ":" + obj[key] + " "
+    }
+    return searchRequestFromString(searchString)
+  }
 }
