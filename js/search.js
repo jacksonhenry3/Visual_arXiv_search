c81a67eac49e94fb25ccb783b6b181bccd039691
Jackson Henry <jacksonhenry3@gmail.com>
2020-10-11 16:51:34 -0400

    increased response delay

--- js/search.js
+++ js/search.js
@@ -1,3 +1,3 @@
 search_bar = d3.select("#search")["_groups"][0][0]
 var res
-function myfunc(){res = searchRequestFromString(search_bar.value);setTimeout(function(){console.log(res["responseJSON"]["response"]["docs"])},50)}
+function myfunc(){res = searchRequestFromString(search_bar.value);setTimeout(function(){console.log(res["responseJSON"]["response"]["docs"])},1000)}
